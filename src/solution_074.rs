7d4a11db8adae34d799a8db7d737e29d63d4d6b1
Jackson Gariety <jackson@gariety.xxx>
2016-04-08 16:24:07 -0700

    A bit cleaner, same speed.

--- src/solution_074.rs
+++ src/solution_074.rs
@@ -1,24 +1,12 @@
 fn factorial(n:u32) -> u32 {
-     return match n {
-         0|1 => 1,
-         2 => 2,
-         3 => 6,
-         4 => 24,
-         5 => 120,
-         6 => 720,
-         7 => 5040,
-         8 => 40320,
-         9 => 362880,
-         _ => 0
-                
-     }
+    [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880][n as usize]
 }
 
-fn digit_factorial_sum(mut arg:u32) -> u32 {
+fn digit_factorial_sum(mut n:u32) -> u32 {
     let mut sum = 0;
-    while arg > 0 {
-        sum += factorial(arg % 10);
-        arg /= 10;
+    while n > 0 {
+        sum += factorial(n % 10);
+        n /= 10;
     }
     sum
 }
@@ -26,10 +14,9 @@ fn digit_factorial_sum(mut arg:u32) -> u32 {
 pub fn solve(max:u32, terms:usize) -> usize {
     (1..max)
         .filter(|&i| {
-            let mut v: Vec<u32> = Vec::with_capacity(terms);
+            let mut v = Vec::with_capacity(terms);
             let mut start = i;
             let mut j = 0;
-            
             while !v.contains(&start) {
                 v.push(start);
                 start = digit_factorial_sum(start);
