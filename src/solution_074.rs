8736e9258c0fee9c5108ffa54ee2585cc870c114
Jackson Gariety <jackson@gariety.xxx>
2016-04-09 02:08:30 -0700

    Adds a solution to #92: ~.50 seconds real!

--- src/solution_074.rs
+++ src/solution_074.rs
@@ -5,23 +5,23 @@ struct FactorialDigitChain {
 
 impl FactorialDigitChain {
     fn new(start: u32, terms: usize) -> FactorialDigitChain {
-        (FactorialDigitChain { n: start, v: Vec::with_capacity(terms) })
+        FactorialDigitChain { n: start, v: Vec::with_capacity(terms) }
     }
+}
+
+fn sum_digit_factorials(mut n: u32) -> u32 {
+    let mut sum = 0;
+    while n > 0 {
+        sum += [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880][(n % 10) as usize]; // use pre-computed factorials 0 through 9
+        n /= 10; // remove one digit from the number
+    }
+    return sum
 }
 
 impl Iterator for FactorialDigitChain {
     type Item = u32;
     
     fn next(&mut self) -> Option<u32> {
-        fn sum_digit_factorials(mut n: u32) -> u32 {
-            let mut sum = 0;
-            while n > 0 {
-                sum += [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880][(n % 10) as usize]; // use pre-computed factorials 0 through 9
-                n /= 10; // remove one digit from the number
-            }
-            return sum
-        }
-
         self.v.push(self.n); // update lifetime
         self.n = sum_digit_factorials(self.n); // calculate n
         if self.v.contains(&self.n) { None } // check the lifetime vector for a duplicate
---
+++ src/solution_092.rs
@@ -0,0 +1,40 @@
+struct SquareDigitChain {
+    first: bool,
+    curr: u32,
+}
+
+fn sum_square_digits(mut n: u32) -> u32 {
+    let mut sum = 0;
+    while n > 0 {
+        sum += [0, 1, 4, 9, 16, 25, 36, 49, 64, 81][(n % 10) as usize]; // use pre-computed squares 0 through 9
+        n /= 10; // remove one digit from the number
+    }
+    return sum
+}
+
+impl SquareDigitChain {
+    fn new(start: u32) -> SquareDigitChain {
+        SquareDigitChain { first: true, curr: start }
+    }
+}
+
+impl Iterator for SquareDigitChain {
+    type Item = u32;
+    
+    fn next(&mut self) -> Option<u32> {            
+        let old_first = self.first;
+        self.first = false;
+
+        if (self.curr == 89 || self.curr == 1) && !old_first { None }
+        else {
+            self.curr = sum_square_digits(self.curr);
+            Some(self.curr)
+        }
+    }
+}
+
+pub fn solve(max:u32, end:u32) -> usize {
+    (1..max) // make a range of each number below that of the problem statement
+        .filter(|&i| SquareDigitChain::new(i).last() == Some(end) )
+        .count() // count the number of items remaining in the range
+}
