9ec8f1f6707f11995a95fd4b765d43770066edcf
Jackson Gariety <jackson@gariety.xxx>
2016-04-10 14:51:44 -0700

    actual time is 78 secs real for #74

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() > 1 {
         if args[1] == "001" { println!("{}", solution_001::solve()); } // ~0.00 seconds real
-        if args[1] == "074" { println!("{}", solution_074::solve(1000000, 60)); } // ~0.83 seconds real
+        if args[1] == "074" { println!("{}", solution_074::solve(1000000, 60)); } // ~0.78 seconds real
         if args[1] == "092" { println!("{}", solution_092::solve(10000000, 89)); } // ~0.83 seconds real
     }
 }
